fd8c67c531961be84cd13be1564d0a0398bfcddb
Michail Mishev <m.mishev1@gmail.com>
2017-12-04 14:56:21 +0200

    Get image recognition result from the v3 model

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -19,12 +19,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
 //        imagePicker.sourceType = .photoLibrary    //using photoLibrary instead of taking photos
-        imagePicker.allowsEditing = false   //later change to true so that the user can crop the image for easier image recognition
+        imagePicker.allowsEditing = false   //later change to true so that the user can crop the image for easier recognition
+        
     }
 
 
@@ -32,12 +34,48 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
             ImageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            
+            detect(image: ciImage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
     }
+    
+    
+    
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+        
+    }
+    
     
     
 
